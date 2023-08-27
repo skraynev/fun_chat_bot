4b05776ac2a18baa9ee78602701237f030ae3137
Sergey Kraynev <sergejyit@gmail.com>
2023-08-27 20:58:39 +0400

    Add flake8 and black tooling

--- eki_game.py
+++ eki_game.py
@@ -1,27 +1,31 @@
-import yaml
 import random
-from os.path import join
-from os import walk
 from collections import namedtuple
+from os import walk
+from os.path import join
 
-TASKS_DIR = 'game_tasks'
-WAIT_START = 'wait'
-IN_PROGRESS = 'in_progress'
+import yaml
 
-Task = namedtuple('Task', ('description', 'time', 'points'))
+TASKS_DIR = "game_tasks"
+WAIT_START = "wait"
+IN_PROGRESS = "in_progress"
+
+Task = namedtuple("Task", ("description", "time", "points"))
 
 
 class EkiGame:
-
     tasks = {
-        1: Task('Поясните слово не используя однокоренные',  60, 1),
-        2: Task('Прочтите слово наоборот', 60, 1),
-        3: Task('Отправьте смайлики или GIF анимации чтобы объяснить слово', 120, 3),
-        4: Task('Вам задают вопросы вы отвечаете только да или нет', 120, 5),
-        5: Task('Издавая ТОЛЬКО звуки пояснить слово', 90, 3),
-        6: Task('Сочинить стих с рифмой на каждое из слов '
-                '(пример: добрый дядя полиглот - утром выпил весь *** (компот))', 120, 4),
-        7: Task('Используя только глаголы пояснить слово', 60, 2),
+        1: Task("Поясните слово не используя однокоренные", 60, 1),
+        2: Task("Прочтите слово наоборот", 60, 1),
+        3: Task("Отправьте смайлики или GIF анимации чтобы объяснить слово", 120, 3),
+        4: Task("Вам задают вопросы вы отвечаете только да или нет", 120, 5),
+        5: Task("Издавая ТОЛЬКО звуки пояснить слово", 90, 3),
+        6: Task(
+            "Сочинить стих с рифмой на каждое из слов "
+            "(пример: добрый дядя полиглот - утром выпил весь *** (компот))",
+            120,
+            4,
+        ),
+        7: Task("Используя только глаголы пояснить слово", 60, 2),
     }
 
     def __init__(self, players=None):
@@ -34,63 +38,83 @@ class EkiGame:
         self.load_data_from_files()
         self.stats = {}
         self.context = {
-            'task': None,
-            'time': None,
-            'points': None,
-            'player_id': None,
-            'player': None,
-            'word': None,
-            'theme': None,
+            "task": None,
+            "time": None,
+            "points": None,
+            "player_id": None,
+            "player": None,
+            "word": None,
+            "theme": None,
         }
 
     def load_data_from_files(self):
-        for (path, _, filenames) in walk(TASKS_DIR):
+        for path, _, filenames in walk(TASKS_DIR):
             for file in filenames:
-                with open(join(path, file), 'r', encoding="utf8") as fi:
+                with open(join(path, file), "r", encoding="utf8") as fi:
                     data = yaml.safe_load(fi)
-                    theme = data.pop('theme')
+                    theme = data.pop("theme")
                     for task_id in data:
-                        self.task_to_words[task_id].extend([
-                            (theme, word)
-                            for word in data[task_id]
-                        ])
+                        self.task_to_words[task_id].extend(
+                            [(theme, word) for word in data[task_id]]
+                        )
 
     def add_player(self, user_id, user_name, first_name):
         if user_id in self.players:
-            return '%s уже в игре' % first_name or user_name
+            return "%s уже в игре" % first_name or user_name
         else:
             self.players[user_id] = first_name
             self.players_by_username[user_name] = user_id
             self.stats[user_id] = 0
-            return '%s присоединился к игре' % first_name or user_name
+            return "%s присоединился к игре" % first_name or user_name
 
     def del_player(self, user_id):
         if user_id in self.players:
             if len(self.players) == 2:
-                return 'Невозможно покинуть игру. Достигнуто минимальное количество игроков.'
-            elif user_id == self.context['player_id'] and self.status == IN_PROGRESS:
-                return 'Невозможно покинуть игру. Текущий игрок должен либо пропустить вопрос, либо ответить.'
+                return (
+                    "Невозможно покинуть игру. Достигнуто минимальное "
+                    "количество игроков."
+                )
+            elif user_id == self.context["player_id"] and self.status == IN_PROGRESS:
+                return (
+                    "Невозможно покинуть игру. Текущий игрок должен либо "
+                    "пропустить вопрос, либо ответить."
+                )
             else:
-                extra_msg = self.next_turn if user_id == self.context['player_id'] else ''
+                extra_msg = (
+                    self.next_turn if user_id == self.context["player_id"] else ""
+                )
                 del self.stats[user_id]
-                return '%s покинул игру\n\n' % self.players.pop(user_id) + extra_msg
+                return "%s покинул игру\n\n" % (self.players.pop(user_id)) + extra_msg
         else:
-            return 'Указанный пользователь не в игре'
+            return "Указанный пользователь не в игре"
 
     @property
     def current_turn(self):
         return (
-        "*Игрок*: {player}\n"
-        "*Задание*: {task}\n"
-        "*Время*: {time} секунд\n"
-        "*Тема*: {theme}"
+            "*Игрок*: {player}\n"
+            "*Задание*: {task}\n"
+            "*Время*: {time} секунд\n"
+            "*Тема*: {theme}"
         ).format(**self.context)
 
     def get_task(self):
         if self.status == IN_PROGRESS:
-            return 'Невозможно запустить задачу - предыдущий вопрос еще не закончился.', None, None, None
+            return (
+                (
+                    "Невозможно запустить задачу - предыдущий вопрос еще "
+                    "не закончился."
+                ),
+                None,
+                None,
+                None,
+            )
         self.status = IN_PROGRESS
-        return None, self.context['player_id'], self.context['word'], self.context['time']
+        return (
+            None,
+            self.context["player_id"],
+            self.context["word"],
+            self.context["time"],
+        )
 
     @property
     def word(self):
@@ -103,15 +127,17 @@ class EkiGame:
             task = self.tasks[task_id]
             word_pair = random.choice(self.task_to_words[task_id])
             player_id = next(self.iter_player)
-            self.context.update({
-                'player': self.players[player_id],
-                'player_id': player_id,
-                'task': task.description,
-                'points': task.points,
-                'time': task.time,
-                'word': word_pair[1],
-                'theme': word_pair[0],
-            })
+            self.context.update(
+                {
+                    "player": self.players[player_id],
+                    "player_id": player_id,
+                    "task": task.description,
+                    "points": task.points,
+                    "time": task.time,
+                    "word": word_pair[1],
+                    "theme": word_pair[0],
+                }
+            )
             self.task_to_words[task_id].remove(word_pair)
             # NOTE: if task type has not more words in section
             # remove this task_id from task_types
@@ -121,7 +147,7 @@ class EkiGame:
             return "Sheet happens on next_turn: %s" % str(exc)
         return self.current_turn
 
-    #def get_players_iterator(self):
+    # def get_players_iterator(self):
     #    if not self.players:
     #        yield None
     #    while True:
@@ -146,7 +172,7 @@ class EkiGame:
         if by_username:
             player_id = self.players_by_username.get(player_id)
         if player_id in self.players:
-            self.stats[player_id] += 1 if by_username else self.context['points']
+            self.stats[player_id] += 1 if by_username else self.context["points"]
             return self.stats[player_id]
         else:
             return False
@@ -159,41 +185,41 @@ class EkiGame:
                 self.stats[player_id] -= 1
                 return self.stats[player_id]
             else:
-                return 'Невозможно сделать количество очков меньше 0'
+                return "Невозможно сделать количество очков меньше 0"
         else:
             return False
 
     @property
     def show_top(self):
-        top_msg = ''
+        top_msg = ""
         for user_id, username in self.players.items():
-            top_msg += '\n%s  ----  %d' % (username, self.stats[user_id])
-        return 'Рейтинг:%s' % top_msg
+            top_msg += "\n%s  ----  %d" % (username, self.stats[user_id])
+        return "Рейтинг:%s" % top_msg
 
     def cancel_question(self):
         if self.status == WAIT_START:
-            return False, 'Нет активного вопроса, который можно пропустить.'
+            return False, "Нет активного вопроса, который можно пропустить."
         self.status = WAIT_START
-        return True, 'Вопрос пропущен!'
+        return True, "Вопрос пропущен!"
 
     def question_is_answered(self):
         if self.status == WAIT_START:
-            return 'Нет активного вопроса, который можно засчитать отгаданным.'
+            return "Нет активного вопроса, который можно засчитать отгаданным."
         self.status = WAIT_START
-        self.add_point(self.context['player_id'])
-        return 'Вопрос отгадан!'
+        self.add_point(self.context["player_id"])
+        return "Вопрос отгадан!"
 
     @staticmethod
     def parse_players_telebot(data):
-        players = data.text.split(' ')
+        players = data.text.split(" ")
         return players
 
     def do_random_sheet(self):
         if random.random() < 0.0000001:
             if random.choice([True, False]):
-                self.add_point(self.context['player_id'])
-                return 'Поздравляем! Вам улабнулась удача и выполучили свое очко'
+                self.add_point(self.context["player_id"])
+                return "Поздравляем! Вам улабнулась удача и выполучили свое очко"
             else:
-                self.remove_point(self.context['player_id'])
-                return 'Сожалеем! Вы утратили свое очко'
-        return ''
+                self.remove_point(self.context["player_id"])
+                return "Сожалеем! Вы утратили свое очко"
+        return ""
--- igruli_bot.py
+++ igruli_bot.py
@@ -1,21 +1,21 @@
 import os
-
-from dotenv import load_dotenv
 import time
-import telebot
-from parse_text_reactions import find_answer_on_text_msg, on_lar_command
-import eki_game
 
+import telebot
+from dotenv import load_dotenv
+
+import eki_game
+from parse_text_reactions import find_answer_on_text_msg, on_lar_command
 
 help_msg = (
-    "Что я умею: \n" 
+    "Что я умею: \n"
     "- supported commands: /help, /lar \n"
     "- реагировать на сообщения с опреленными словами \n"
     "- показывать желтую карточку в некоторых случаях \n"
 )
 
 load_dotenv()
-bot = telebot.TeleBot(os.environ.get('TELE_TOKEN'))
+bot = telebot.TeleBot(os.environ.get("TELE_TOKEN"))
 count_lar = 0
 eki_games = {}
 admin_ids = []
@@ -23,12 +23,12 @@ admin_ids = []
 game_cls = eki_game.EkiGame()
 
 
-@bot.message_handler(commands=['help'])
+@bot.message_handler(commands=["help"])
 def help_message(message):
     bot.send_message(message.chat.id, help_msg)
 
 
-@bot.message_handler(commands=['lar'])
+@bot.message_handler(commands=["lar"])
 def emulate_lar(message):
     global count_lar
     count_lar, answer = on_lar_command(count_lar)
@@ -38,93 +38,121 @@ def emulate_lar(message):
 def game_is_run(message):
     chat_id = message.chat.id
     if chat_id not in eki_games:
-        bot.send_message(chat_id, 'Нет запущенной игры')
+        bot.send_message(chat_id, "Нет запущенной игры")
         return False
     return chat_id
+
 
 def is_admin(message):
     if message.from_user.id not in admin_ids:
         chat_id = message.chat.id
-        bot.send_message(chat_id, 'У вас нет прав для этой команды!')
+        bot.send_message(chat_id, "У вас нет прав для этой команды!")
         return False
     return True
 
 
-help_msg = '''
+help_msg = """
 Игра создана!
 
 ВАЖНО:
- - Для корректной работы необходимо один раз отправить сообщение любого содержания боту: @%s
- - Для начала хода необходимо минимум 2-е участников
- - Для начисления очков администратором укажите username в настройках аккаунта
+- Для корректной работы необходимо один раз отправить сообщение любого
+  содержания боту: @%s
+- Для начала хода необходимо минимум 2-е участников
+- Для начисления очков администратором укажите username в настройках аккаунта
 
 Используя команды ниже, присоединяйтесь и играйте.
 - Присоединится к игре, нажмите - /join
 - Покинуть игру, нажмите - /drop_me
-- Показать список игроков и результаты, нажмите - /top 
+- Показать список игроков и результаты, нажмите - /top
 - Показать, кто ходит следующий и какое задание - /whoisnext
 - Получить задание текущему игроку и запустить таймер задания - /go
 
 Только для администратора игры:
 - Создание игры - /game
-- Добавить очко игроку - /add_point 
-- Отнять очко игроку - /remove_point 
+- Добавить очко игроку - /add_point
+- Отнять очко игроку - /remove_point
 - Пропустить текущий ход и перeйти к следующему игроку, нажмите - /cancel
 - Засчитать вопрос отгаданным - /win
 - Завершить игру и вывести результат, нажмите - /exit
-'''
+"""
 
-@bot.message_handler(commands=['game_help'])
-def create_eki_game(message):
+
+@bot.message_handler(commands=["game_help"])
+def help_eki_game(message):
     bot.send_message(message.chat.id, help_msg % bot.get_me().username)
 
 
-@bot.message_handler(commands=['game'])
+@bot.message_handler(commands=["game"])
 def create_eki_game(message):
     if not is_admin(message):
         return
     global eki_games
     chat_id = message.chat.id
     if chat_id in eki_games:
-        bot.send_message(chat_id, 'Игра уже запущена')
+        bot.send_message(chat_id, "Игра уже запущена")
         return
     game_cls = eki_game.EkiGame()
     eki_games = {chat_id: game_cls}
     bot.send_message(chat_id, help_msg % bot.get_me().username)
 
 
-@bot.message_handler(commands=['join'])
+@bot.message_handler(commands=["join"])
 def join_to_game(message):
     chat_id = game_is_run(message)
     if not chat_id:
         return
     game_cls = eki_games[chat_id]
     user = message.from_user
-    bot.send_message(chat_id, game_cls.add_player(user.id, user.username, user.first_name))
+    bot.send_message(
+        chat_id, game_cls.add_player(user.id, user.username, user.first_name)
+    )
     try:
-        bot.send_message(user.id, 'Вы присоединились к игре. В этот чат будут присылаться слова для заданий.')
+        bot.send_message(
+            user.id,
+            (
+                "Вы присоединились к игре. В этот чат будут "
+                "присылаться слова для заданий."
+            ),
+        )
         if 1 < len(game_cls.players) < 3:
-            run_msg = 'Когда игрок будет готов получите задание выполнив команду - /go'
-            bot.send_message(chat_id, "%s\n\n%s" % (game_cls.next_turn, run_msg), parse_mode="Markdown")
+            run_msg = (
+                "Когда игрок будет готов получите задание выполнив команду - " "/go"
+            )
+            bot.send_message(
+                chat_id,
+                "%s\n\n%s" % (game_cls.next_turn, run_msg),
+                parse_mode="Markdown",
+            )
     except telebot.apihelper.ApiException as exc:
         json_res = exc.result.json()
-        if (json_res['error_code'] == 403
-                and json_res['description'] == "Forbidden: bot can't initiate conversation with a user"):
-            bot.send_message(chat_id, 'Отправьте любое сообщение боту @%s. Затем выполните /drop_me и /join команды.' % bot.get_me().username)
+        if (
+            json_res["error_code"] == 403
+            and json_res["description"]
+            == "Forbidden: bot can't initiate conversation with a user"
+        ):
+            bot.send_message(
+                chat_id,
+                (
+                    "Отправьте любое сообщение боту @%s. "
+                    "Затем выполните /drop_me и /join команды."
+                ) % bot.get_me().username,
+            )
         else:
             bot.send_message(chat_id, "SERVER TELEGRAM ERROR: %s" % json_res)
 
 
-@bot.message_handler(commands=['drop_me'])
+@bot.message_handler(commands=["drop_me"])
 def leave_game(message):
     chat_id = game_is_run(message)
     if not chat_id:
         return
     game_cls = eki_games[chat_id]
-    bot.send_message(chat_id, game_cls.del_player(message.from_user.id), parse_mode="Markdown")
+    bot.send_message(
+        chat_id, game_cls.del_player(message.from_user.id), parse_mode="Markdown"
+    )
 
 
-@bot.message_handler(commands=['top'])
+@bot.message_handler(commands=["top"])
 def show_top(message):
     chat_id = game_is_run(message)
     if not chat_id:
@@ -132,14 +160,18 @@ def show_top(message):
     game_cls = eki_games[chat_id]
     bot.send_message(chat_id, game_cls.show_top)
 
-@bot.message_handler(commands=['go'])
+
+@bot.message_handler(commands=["go"])
 def send_question(message):
     chat_id = game_is_run(message)
     if not chat_id:
         return
     game_cls = eki_games[chat_id]
     if len(game_cls.players) < 2:
-        bot.send_message(chat_id, "Недостаточно игроков для игры. Минимально разрешенное количество - 2.")
+        bot.send_message(
+            chat_id,
+            "Недостаточно игроков для игры. Минимально разрешенное количество - 2.",
+        )
         return
 
     err_msg, player_id, word, task_time = game_cls.get_task()
@@ -147,70 +179,93 @@ def send_question(message):
     if err_msg is None:
         try:
             bot.send_message(player_id, word)
-            bot.send_message(chat_id, 'Задание отправлено игроку: %s' % game_cls.context['player'])
+            bot.send_message(
+                chat_id, "Задание отправлено игроку: %s" % game_cls.context["player"]
+            )
         except Exception as exc:
-            bot.send_message(chat_id, 'Sheet happens: %s' % str(exc))
+            bot.send_message(chat_id, "Sheet happens: %s" % str(exc))
             ret_status, msg = game_cls.cancel_question()
             bot.send_message(chat_id, msg)
             if ret_status:
-                run_msg = 'Когда игрок будет готов получите задание выполнив команду - /go'
-                bot.send_message(chat_id, "%s\n\n%s" % (game_cls.next_turn, run_msg), parse_mode="Markdown")
+                run_msg = (
+                    "Когда игрок будет готов получите задание выполнив команду - /go"
+                )
+                bot.send_message(
+                    chat_id,
+                    "%s\n\n%s" % (game_cls.next_turn, run_msg),
+                    parse_mode="Markdown",
+                )
             return
     else:
         bot.send_message(chat_id, err_msg)
         return
     # ЗАПУСТИТЬ ТАЙМЕР!!!!
-    bot.send_message(chat_id, 'Таймер запущен!\nПри победе нажмите /win\nДля пропуска - /cancel')
+    bot.send_message(
+        chat_id, "Таймер запущен!\nПри победе нажмите /win\nДля пропуска - /cancel"
+    )
     for _ in range(10 * task_time):
         time.sleep(0.1)
         # check, may be it was skipped manually
         if game_cls.status == eki_game.WAIT_START:
-            bot.send_message(chat_id, 'Таймер сброшен.')
+            bot.send_message(chat_id, "Таймер сброшен.")
             break
     else:
         # ОКОНЧАНИЕ таймера и вопроса
-        bot.send_message(chat_id, 'Время вышло. %s' % game_cls.cancel_question()[1])
-        run_msg = 'Когда игрок будет готов получите задание выполнив команду - /go'
-        bot.send_message(chat_id, "%s\n\n%s" % (game_cls.next_turn, run_msg), parse_mode="Markdown")
+        bot.send_message(chat_id, "Время вышло. %s" % game_cls.cancel_question()[1])
+        run_msg = "Когда игрок будет готов получите задание выполнив команду - /go"
+        bot.send_message(
+            chat_id, "%s\n\n%s" % (game_cls.next_turn, run_msg), parse_mode="Markdown"
+        )
 
-@bot.message_handler(commands=['whoisnext'])
+
+@bot.message_handler(commands=["whoisnext"])
 def who_is_next(message):
     chat_id = game_is_run(message)
     if not chat_id:
         return
     game_cls = eki_games[chat_id]
-    run_msg = 'Когда игрок будет готов запустите таймер и получите задание выполнив команду - /go'
-    bot.send_message(chat_id, '%s\n\n%s' % (game_cls.current_turn, run_msg), parse_mode="Markdown")
+    run_msg = (
+        "Когда игрок будет готов запустите таймер и получите задание выполнив "
+        "команду - /go"
+    )
+    bot.send_message(
+        chat_id, "%s\n\n%s" % (game_cls.current_turn, run_msg), parse_mode="Markdown"
+    )
 
 
-@bot.message_handler(commands=['cancel'])
+@bot.message_handler(commands=["cancel"])
 def cancel_question(message):
     chat_id = game_is_run(message)
     if not chat_id:
         return
     game_cls = eki_games[chat_id]
-    if not is_admin(message) or message.from_user.id != game_cls.context['player_id']:
+    if not is_admin(message) or message.from_user.id != game_cls.context["player_id"]:
         return
     ret_status, msg = game_cls.cancel_question()
     bot.send_message(chat_id, msg)
     if ret_status:
-        run_msg = 'Когда игрок будет готов получите задание выполнив команду - /go'
-        bot.send_message(chat_id, "%s\n\n%s" % (game_cls.next_turn, run_msg), parse_mode="Markdown")
+        run_msg = "Когда игрок будет готов получите задание выполнив команду - /go"
+        bot.send_message(
+            chat_id, "%s\n\n%s" % (game_cls.next_turn, run_msg), parse_mode="Markdown"
+        )
 
-@bot.message_handler(commands=['win'])
+
+@bot.message_handler(commands=["win"])
 def win_question(message):
     chat_id = game_is_run(message)
     if not chat_id:
         return
     game_cls = eki_games[chat_id]
-    if not is_admin(message) or message.from_user.id != game_cls.context['player_id']:
+    if not is_admin(message) or message.from_user.id != game_cls.context["player_id"]:
         return
     bot.send_message(chat_id, game_cls.question_is_answered())
-    run_msg = 'Когда игрок будет готов получите задание выполнив команду - /go'
-    bot.send_message(chat_id, "%s\n\n%s" % (game_cls.next_turn, run_msg), parse_mode="Markdown")
+    run_msg = "Когда игрок будет готов получите задание выполнив команду - /go"
+    bot.send_message(
+        chat_id, "%s\n\n%s" % (game_cls.next_turn, run_msg), parse_mode="Markdown"
+    )
 
 
-@bot.message_handler(commands=['exit'])
+@bot.message_handler(commands=["exit"])
 def del_eki_game(message):
     chat_id = game_is_run(message)
     if not chat_id:
@@ -218,11 +273,11 @@ def del_eki_game(message):
     if not is_admin(message):
         return
     game_cls = eki_games.pop(chat_id, None)
-    bot.send_message(chat_id, 'Игра Закончена! \n%s' % game_cls.show_top)
+    bot.send_message(chat_id, "Игра Закончена! \n%s" % game_cls.show_top)
     del game_cls
 
 
-@bot.message_handler(commands=['add_point'])
+@bot.message_handler(commands=["add_point"])
 def add_point(message):
     chat_id = game_is_run(message)
     if not chat_id:
@@ -231,18 +286,19 @@ def add_point(message):
         return
 
     game_cls = eki_games[chat_id]
-    command_and_username = message.text.split('@')
+    command_and_username = message.text.split("@")
     if len(command_and_username) != 2:
-        bot.send_message(chat_id, 'Не указан username игрока')
+        bot.send_message(chat_id, "Не указан username игрока")
         return
     username = command_and_username[1]
     result = game_cls.add_point(username, by_username=True)
     if result:
-        bot.send_message(chat_id, '%s' % game_cls.show_top)
+        bot.send_message(chat_id, "%s" % game_cls.show_top)
     else:
-        bot.send_message(chat_id, 'Не удалось найти игрока с таким username')
+        bot.send_message(chat_id, "Не удалось найти игрока с таким username")
 
-@bot.message_handler(commands=['remove_point'])
+
+@bot.message_handler(commands=["remove_point"])
 def remove_point(message):
     chat_id = game_is_run(message)
     if not chat_id:
@@ -251,19 +307,20 @@ def remove_point(message):
         return
 
     game_cls = eki_games[chat_id]
-    command_and_username = message.text.split('@')
+    command_and_username = message.text.split("@")
     if len(command_and_username) != 2:
-        bot.send_message(chat_id, 'Не указан username игрока')
+        bot.send_message(chat_id, "Не указан username игрока")
         return
     username = command_and_username[1]
     result = game_cls.remove_point(username, by_username=True)
     if result:
-        bot.send_message(chat_id, '%s' % game_cls.show_top)
+        bot.send_message(chat_id, "%s" % game_cls.show_top)
     else:
-        bot.send_message(chat_id, 'Не удалось найти игрока с таким username')
+        bot.send_message(chat_id, "Не удалось найти игрока с таким username")
 
-#@bot.message_handler(commands=['admin'])
-#def show_top(message):
+
+# @bot.message_handler(commands=['admin'])
+# def show_top(message):
 #    chat_id = game_is_run(message)
 #    if not chat_id:
 #        return
@@ -274,21 +331,21 @@ def remove_point(message):
 #    bot.send_message(chat_id, game_cls.show_top)
 
 
-@bot.message_handler(content_types=['text'])
+@bot.message_handler(content_types=["text"])
 def send_text(message):
     chat_id = message.chat.id
     msg_text = message.text.lower()
 
     if chat_id in eki_games:
-        game_cls = eki_games[chat_id]
+        pass
         # TODO: add parsing of answers
-        #if game_cls.status == eki_game.IN_PROGRESS:
+        # game_cls = eki_games[chat_id]
+        # if game_cls.status == eki_game.IN_PROGRESS:
         #    game_cls.parse_answer(msg_text)
     else:
         answer = find_answer_on_text_msg(msg_text, message.from_user.username)
         if answer:
             bot.send_message(chat_id, answer)
-
 
 
 bot.polling()
